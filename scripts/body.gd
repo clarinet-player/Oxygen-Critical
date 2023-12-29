8eb76847571b29ef135a5773d4957ea94f43a09a
clarinet-player <100056739+clarinet-player@users.noreply.github.com>
2023-12-29 15:05:34 -0800

    work2

--- scripts/body.gd
+++ scripts/body.gd
@@ -1,6 +1,12 @@
 extends RigidBody3D
 
-#func _input(event):
-##	if event is InputEventKey and event.pressed:
-#		if event.keycode == KEY_T:
-#			reparent($"../..")
+func _input(event):
+	if event is InputEventKey and event.pressed:
+		if event.keycode == KEY_T:
+			var anchored_body = get_parent()
+			var offset : Vector3 = position
+			
+			reparent($"../..")
+			
+			apply_central_impulse( anchored_body.linear_velocity )
+			apply_central_impulse( position.cross( anchored_body.angular_velocity ) )
--- scripts/testcentrifuge.gd
+++ scripts/testcentrifuge.gd
@@ -1,6 +1,6 @@
-extends Node3D
+extends RigidBody3D
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	rotate_z(0.1)
+func _ready():
+	angular_velocity = Vector3(0, 0, 2)
+
