3b411d248c22d2c29d0cdce2c2dbafece0fb52ab
clarinet-player <100056739+clarinet-player@users.noreply.github.com>
2024-04-05 21:27:57 -0700

    bullet damage fix

--- scripts/ship.gd
+++ scripts/ship.gd
@@ -1,6 +1,6 @@
 extends RigidBody3D
 
-var centrifuge_angular_velocity := Vector3(0.3, 0, 0)
+var centrifuge_angular_velocity := Vector3(0.1, 0, 0)
 
 func _physics_process(delta):
 	global_rotate(centrifuge_angular_velocity.normalized(), centrifuge_angular_velocity.length() * delta)
