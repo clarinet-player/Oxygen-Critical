a3f01113a1ff2247280d6a9611308ed50f83706d
clarinet-player <100056739+clarinet-player@users.noreply.github.com>
2024-08-08 16:10:05 -0700

    yo yo yo

--- classes/weaponbase.gd
+++ classes/weaponbase.gd
@@ -219,8 +219,9 @@ func fire(pos : Vector3, velocity : Vector3):
 	if get_parent():
 		var case = preload("res://classes/casing.tscn").instantiate()
 		case.type = casing
-		case.velocity = ejection.global_basis.z.normalized() * 4 + Vector3(randf_range(-0.3, 0.3), randf_range(-0.3, 0.3), randf_range(-0.3, 0.3)) + get_parent().get_parent().velocity
+		case.velocity = ejection.global_basis.z.normalized() * 5 + Vector3(randf_range(-0.3, 0.3), randf_range(-0.3, 0.3), randf_range(-0.3, 0.3)) + get_parent().get_parent().velocity
 		case.angular = Vector3(randf_range(-0.5, 0.5), randf_range(-0.5, 0.5), randf_range(-0.5, 0.5))
 		Gamemanager.add_child(case)
 		case.global_position = ejection.global_position
 		case.look_at(case.global_position + velocity)
+		case.global_rotate(case.angular.normalized(), 0.25)
